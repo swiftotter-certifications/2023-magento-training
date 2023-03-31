4afa822f023e10855d4647cb6a50442e5f1cad33
Łukasz Bajsarowicz <lukasz@swiftotter.com>
2023-03-31 15:54:24 +0200

    2023-03-28 - Hands-on workshops

---
+++ app/code/SwiftOtter/Training/Controller/Index/Index.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+namespace SwiftOtter\Training\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ResponseFactory;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NotFoundException;
+
+class Index implements HttpGetActionInterface
+{
+
+    /**
+     * @var \Magento\Framework\Controller\ResultFactory
+     */
+    private ResultFactory $resultFactory;
+
+    public function __construct(ResultFactory $resultFactory)
+    {
+        $this->resultFactory = $resultFactory;
+    }
+
+    /**
+     * Execute action based on request and return result
+     * @return ResultInterface|ResponseInterface
+     * @throws NotFoundException
+     */
+    public function execute()
+    {
+        return $this->resultFactory->create(
+            \Magento\Framework\Controller\ResultFactory::TYPE_PAGE
+        );
+    }
+}

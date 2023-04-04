f6576bc1b75f6da3777fc321e2e5e90775d9b870
Łukasz Bajsarowicz <lukasz@swiftotter.com>
2023-04-04 18:34:19 +0200

    2023-04-04 - Controllers Demo

--- app/code/SwiftOtter/Training/Controller/Index/Index.php
+++ app/code/SwiftOtter/Training/Controller/Index/Index.php
@@ -3,15 +3,15 @@ declare(strict_types=1);
 namespace SwiftOtter\Training\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\ResponseFactory;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\NotFoundException;
 
-class Index implements HttpGetActionInterface
+class Index implements \Magento\Framework\App\ActionInterface
 {
-
     /**
      * @var \Magento\Framework\Controller\ResultFactory
      */
